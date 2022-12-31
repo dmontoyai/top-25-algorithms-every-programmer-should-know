a865927b67ce03b8199d444ba03e4620664453c3
Pedro Lopez Mareque <pedro.lopez.mareque@gmail.com>
2022-12-31 08:20:30 +0100

    feat: make binary search generic

--- src/binary_search.py
+++ src/binary_search.py
@@ -1,3 +1,8 @@
+from typing import TypeVar, Generic
+
+T = TypeVar('T', str, int)
+
+
 # Given a sorted array of N integers and a target value,
 # determine if the target exists in the array in logarithmic
 # time using the binary search algorithm.
@@ -5,10 +10,10 @@
 # If target exists in the array, print the index of it.
 #
 # Complexity:	O(log N) -> log2(N)
-class BinarySearch:
+class BinarySearch(Generic[T]):
 
     @staticmethod
-    def solve(stack: list, target: int) -> int:
+    def solve(stack: list[T], target: T) -> int:
         steps = 0
         left = 0
         right = len(stack) - 1
--- tests/test_binary_search.py
+++ tests/test_binary_search.py
@@ -1,5 +1,4 @@
 import math
-import pytest
 
 from expects import equal, expect
 
@@ -8,22 +7,24 @@ from src.binary_search import BinarySearch
 
 class TestBinarySearch:
 
-    @pytest.fixture
-    def stack(self) -> list:
-        return list(range(16))
+    def test_worst_scenario(self) -> None:
+        stack = [chr(97 + index) for index in range(16)]
 
-    def test_worst_scenario(self, stack: list) -> None:
-        steps = BinarySearch.solve(stack, 0)
+        steps = BinarySearch.solve(stack, "a")
 
         expected_steps = int(math.sqrt(len(stack)))
         expect(steps).to(equal(expected_steps))
 
-    def test_best_scenario(self, stack: list) -> None:
+    def test_best_scenario(self) -> None:
+        stack = list(range(16))
+
         steps = BinarySearch.solve(stack, 7)
 
         expect(steps).to(equal(1))
 
-    def test_search_the_last_element_in_the_list(self, stack: list) -> None:
-        steps = BinarySearch.solve(stack, 15)
+    def test_search_the_last_element_in_the_list(self) -> None:
+        stack = [chr(97 + index) for index in range(16)]
+        steps = BinarySearch.solve(stack, "p")
+        print(stack)
 
         expect(steps).to(equal(5))
