c63e7b7dfd9641b4d00c1cdbd1e878f3cde6f7db
Pedro Lopez Mareque <pedro.lopez.mareque@gmail.com>
2023-01-03 21:18:51 +0100

    feat: solved BubbleSort

---
+++ src/bubble_sort.py
@@ -0,0 +1,25 @@
+from typing import Generic, TypeVar
+
+T = TypeVar('T', str, int)
+
+
+# BubbleSort is an in-place comparison sorting algorithm.
+#
+# It repeatedly steps through the input list element by element,
+# comparing the current element with the one after it,
+# swapping their values if needed.
+#
+# Complexity: O(n^2).
+class BubbleSort(Generic[T]):
+
+    def solve(self, items: list[T]) -> list[T]:
+        size = len(items)
+        swapped = False
+        for idx in range(size - 1):
+            for idy in range(0, size - idx - 1):
+                if items[idy] > items[idy + 1]:
+                    swapped = True
+                    items[idy], items[idy + 1] = items[idy + 1], items[idy]
+            if not swapped:
+                return items
+        return items
---
+++ tests/test_bubble_sort.py
@@ -0,0 +1,22 @@
+from expects import equal, expect
+
+from src.bubble_sort import BubbleSort
+
+
+class TestBubbleSort:
+
+    def test_sort_an_array(self) -> None:
+        array = [10, 5, 1, 3]
+        bubble_sort = BubbleSort()
+
+        sorted_array = bubble_sort.solve(array)  # type: ignore
+
+        expect(sorted_array).to(equal([1, 3, 5, 10]))
+
+    def test_sort_an_array_of_letters(self) -> None:
+        array = ["z", "h", "a", "f", "x"]
+        bubble_sort = BubbleSort()
+
+        sorted_array = bubble_sort.solve(array)
+
+        expect(sorted_array).to(equal(["a", "f", "h", "x", "z"]))
