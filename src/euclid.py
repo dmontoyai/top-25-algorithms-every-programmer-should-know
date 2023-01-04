3175bb854987f85699688bbe968f400a0d3faf39
Pedro Lopez Mareque <pedro.lopez.mareque@gmail.com>
2023-01-04 21:14:40 +0100

    fix build

--- src/euclid.py
+++ src/euclid.py
@@ -4,7 +4,7 @@
 # Complexity: O(log(min(a, b)).
 class Euclid:
 
-    def solve(self, a: int, b: int) -> int:
-        if a == 0:
-            return b
-        return self.solve(b % a, a)
+    def solve(self, first_number: int, second_number: int) -> int:
+        if first_number == 0:
+            return second_number
+        return self.solve(second_number % first_number, first_number)
--- tests/test_euclid.py
+++ tests/test_euclid.py
@@ -5,13 +5,14 @@ from src.euclid import Euclid
 
 class TestEuclid:
 
-    @pytest.mark.parametrize("a,b,result", [
+    @pytest.mark.parametrize("first_number,second_number,result", [
         (10, 15, 5),
         (35, 10, 5),
         (31, 2, 1),
     ])
-    def test_find_gcd(self, a: int, b: int, result: int) -> None:
+    def test_find_gcd(self, first_number: int, second_number: int,
+                      result: int) -> None:
         euclid = Euclid()
-        gcd = euclid.solve(a, b)
+        gcd = euclid.solve(first_number, second_number)
 
         expect(gcd).to(equal(result))
